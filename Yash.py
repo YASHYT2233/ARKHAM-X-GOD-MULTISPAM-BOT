bed9854c9c599a401772cee3e829246962923cec
ArkhamXGod <92201185+YASHYT2233@users.noreply.github.com>
2022-01-06 22:17:20 +0530

    Update Yash.py

--- Yash.py
+++ Yash.py
@@ -1016,7 +1016,6 @@ async def gifspam(e, smex):
 @raj.on(events.NewMessage(incoming=True, pattern=r"\*bio"))
 @put.on(events.NewMessage(incoming=True, pattern=r"\*bio"))
 @eag.on(events.NewMessage(incoming=True, pattern=r"\*bio"))
-@gle.on(events.NewMessage(incoming=True, pattern=r"\*bio"))
 @wal.on(events.NewMessage(incoming=True, pattern=r"\*bio"))
 @aaa.on(events.NewMessage(incoming=True, pattern=r"\*bio"))
 @boy.on(events.NewMessage(incoming=True, pattern=r"\*bio"))
